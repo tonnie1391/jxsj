cee33c92e1435b739d72bb7b91f5d0366a15ddec
jxsj <tonnie1391@gmail.com>
2022-09-13 22:58:31 +0700

    Update task_def.txt

--- GS/script/npc/liguan.lua
+++ GS/script/npc/liguan.lua
@@ -1,52 +1,58 @@
 local tbLiGuan = Npc:GetClass("liguan");
 
-tbLiGuan.nTaskGroupId	= 2093;
-tbLiGuan.nTaskId		= 78;
+tbLiGuan.nTaskGroupId		= 2211;
+tbLiGuan.nTaskIdDomain		= 1;
+tbLiGuan.nTaskIdMoney		= 2;
+
+tbLiGuan.receiveAwardDate	= 20211010;
 
 function tbLiGuan:OnDialog()
 	DoScript("\\script\\npc\\liguan.lua")
 	local szMsg = "Chào đại hiệp, người cần gì?";
 	local tbOpt = 
 	{
-		-- {"Đền bù bảo trì đột xuất", self.baotri, self},
 		{"Phúc lợi", self.FuLi, self},
 		{"Đoán Hoa Đăng", GuessGame.OnDialog, GuessGame},
 		{"Kết thúc đối thoại"},
 	}
-
+	
+	local currentDate = tonumber(os.date("%Y%m%d", GetTime()));
+	if currentDate >= self.receiveAwardDate then
+		table.insert(tbOpt, 1, {"<color=yellow>Sự kiện Đua Top<color>", self.receiveAwardTop, self,});
+	end
+	
 	Dialog:Say(szMsg, tbOpt);
 end
 
-function tbLiGuan:baotri()
-	local tbOpt = 
-	{
-		{"29/5/2022", self.nhanvatphamdenbu, self, 1},
+function tbLiGuan:receiveAwardTop()
+	local tbOpt = {
+		{"Nhận thưởng Top Lãnh Thổ", self.sendAwardTop, self, 1},
+		{"Xem Top Lãnh Thổ", self.sendAwardTop, self, 1},
+		{"Nhận thưởng Top Tài Phú", self.sendAwardTop, self, 2},
+		{"Xem Top Tài Phú", self.sendAwardTop, self, 2},
 		{"Kết thúc đối thoại"},
 	}
-	Dialog:Say("Hãy chọn phần thưởng:");
+
+	Dialog:Say("Hãy chọn điều người cần", tbOpt);          
 end
 
-function tbLiGuan:nhanvatphamdenbu()
+function tbLiGuan:sendAwardTop(nType, nSure)
+	if not nSure then
 	
-
-
+	else
+	
+	end
 end
 
 function tbLiGuan:FuLi()
-	-- me.AddItem(18,1,16,1)
-	local tbOpt = 
-	{
+	local tbOpt = {
 		{"Mua Tinh Hoạt phúc lợi (tiểu)", SpecialEvent.BuyJingHuo.OnDialog, SpecialEvent.BuyJingHuo, 1},
 		{"Mua Tinh Hoạt phúc lợi (trung)", SpecialEvent.BuyJingHuo.OnDialog, SpecialEvent.BuyJingHuo, 2},
 		{"Mua Tinh Hoạt phúc lợi (đại)", SpecialEvent.BuyJingHuo.OnDialog, SpecialEvent.BuyJingHuo, 3},
 		{"Đổi bạc khóa", SpecialEvent.CoinExchange.OnDialog, SpecialEvent.CoinExchange},
 		{"Nhận lương", SpecialEvent.Salary.GetSalary, SpecialEvent.Salary},
+		{"Kết thúc đối thoại"},
 	}
 
-	if SpecialEvent.NewPlayerGift:ShowOption()==1 then
-		table.insert(tbOpt, {"Túi tân thủ", SpecialEvent.NewPlayerGift.OnDialog, SpecialEvent.NewPlayerGift});
-	end
-	
-	table.insert(tbOpt, {"Kết thúc đối thoại"});
 	Dialog:Say("Hãy chọn điều người cần", tbOpt);          
 end
